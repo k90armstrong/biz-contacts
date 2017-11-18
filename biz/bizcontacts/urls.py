204f022c0540db42d1c96837f09748eac0d9815f
Wade Wilkey <wadewilkey@Wades-MacBook-Pro.local>
2017-11-18 13:11:56 -0700

    Added logout

--- biz/bizcontacts/urls.py
+++ biz/bizcontacts/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^dashboard/$', views.dashboard, name='index'),
-    url(r'^signup/$', views.signup, name='index'),
-    url(r'^login/$', views.login_view, name='index')    
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout')    
 ]
--- biz/bizcontacts/views.py
+++ biz/bizcontacts/views.py
@@ -48,6 +48,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    return redirect(reverse('index'))
     # redirect to the home page?
 
 
