3b97263eaf2ab53833b03fcbdc08a7f51c6f8b4c
Kyle <k90armstrong@gmail.com>
2017-11-18 12:58:41 -0700

    start of add new contact modal

--- biz/bizcontacts/static/bizcontacts/javascript/app.js
+++ biz/bizcontacts/static/bizcontacts/javascript/app.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     // global variables
     var rightMenu = {state: 'close'};
+    var newContactModal = {
+        state: 'close', 
+        el: document.getElementById('new-contact-modal')
+    };
 
     // functions
     function openRightMenu() {
@@ -15,6 +19,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
         rightMenu.state = 'close';
     }
 
+    function openModal(modal) {
+        console.log(modal);
+        modal.el.className = 'modal is-active';
+        modal.state = 'open';
+    }
+
+    function closeModal(modal) {
+        console.log(modal);
+        modal.el.className = 'modal';
+        modal.state = 'close';
+    }
+
     // handlers
     function rightMenuHandler(event) {
         event.stopPropagation();
@@ -22,15 +38,28 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     function documentClickHandler(event) {
-        if (rightMenu.state == 'open') {
-            console.log('close');
+        if (rightMenu.state === 'open') {
             closeRightMenu();
         }
+        if (newContactModal.state === 'open') {
+            closeModal(newContactModal);
+            console.log(event);
+        }
+    }
+
+    function addNewContactHandler(event) {
+        event.stopPropagation();        
+        openModal(newContactModal);
+    }
+
+    function modalClickHandler(event) {
+        event.stopPropagation();
     }
 
     // add event listener to the join and login
     document.getElementById('right-menu').addEventListener('click', rightMenuHandler);
     document.addEventListener('click', documentClickHandler);
-
+    document.getElementById('new-contact-button').addEventListener('click', addNewContactHandler);
+    $('.modal-content').on('click', modalClickHandler);
 
 });
